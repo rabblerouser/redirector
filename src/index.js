cac0a1a9a7d4d23b7b1cfd68d3983eca6ce559ee
Jared Willett <jaredrwillett@gmail.com>
2018-05-03 15:24:08 +1000

    get requests now publish click events before redirecting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
 const express = require('express');
+const sendClickEvent = require('./sendClickEvent');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	if (req.query.outboundUrl) {
+		sendClickEvent(req.query.outboundUrl)
+			.catch(() => console.log("Failed to publish click event"));
 		res.redirect(req.query.outboundUrl);
 	} else {
 		res.status(400).send({ error: "No URL provided to redirect to" });

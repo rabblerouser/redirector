6ff26aa8adee7d8e1403d2b40cfa4bb5e8cbf7d6
Jared Willett <jaredrwillett@gmail.com>
2018-05-04 12:37:19 +1000

    add describe to tests

--- src/__tests__/handleRedirectRequestTest.js
+++ src/__tests__/handleRedirectRequestTest.js
@@ -6,24 +6,26 @@ const sinon = require('sinon');
 chai.use(require('sinon-chai'));
 var expect = chai.expect;
 
-it('redirects an incoming request when an outbound URL is provided', () => {
-	const testOutboundUrl = 'http://test.com';
-	const req = { query: { outboundUrl: 'http://test.com' }};
-	const res = { redirect: sinon.stub() };
+describe('handleRedirectRequest', () => {
+	it('redirects an incoming request when an outbound URL is provided', () => {
+		const testOutboundUrl = 'http://test.com';
+		const req = { query: { outboundUrl: 'http://test.com' }};
+		const res = { redirect: sinon.stub() };
 
-	handleRedirectRequest(req, res);
-	
-	expect(res.redirect).to.have.been.calledWith(testOutboundUrl);
-});
+		handleRedirectRequest(req, res);
+		
+		expect(res.redirect).to.have.been.calledWith(testOutboundUrl);
+	});
 
-it('sends a 400 error if an outbound URL is not provided', () => {
-	const testOutboundUrl = 'http://test.com';
-	let res = { status: sinon.stub(), send: sinon.stub() };
-	res.status.returns(res);
-	const req = { query: {} };
+	it('sends a 400 error if an outbound URL is not provided', () => {
+		const testOutboundUrl = 'http://test.com';
+		let res = { status: sinon.stub(), send: sinon.stub() };
+		res.status.returns(res);
+		const req = { query: {} };
 
-	handleRedirectRequest(req, res);
-	
-	expect(res.status).to.have.been.calledWith(400);
-	expect(res.send).to.have.been.calledWith({ error: "No URL provided to redirect to" });
+		handleRedirectRequest(req, res);
+		
+		expect(res.status).to.have.been.calledWith(400);
+		expect(res.send).to.have.been.calledWith({ error: "No URL provided to redirect to" });
+	});
 });
--- src/__tests__/sendClickEventTest.js
+++ src/__tests__/sendClickEventTest.js
@@ -6,15 +6,18 @@ const sinon = require('sinon');
 chai.use(require('sinon-chai'));
 var expect = chai.expect;
 
-it('publishes a link-clicked event', () => {
-	const testOutboundUrl = 'http://test.com';
-	const eventBody = {outboundUrl: 'http://test.com'};
+describe('sendClickEvent', () => {
+	it('publishes a link-clicked event', () => {
+		const testOutboundUrl = 'http://test.com';
+		const eventBody = {outboundUrl: 'http://test.com'};
 
-	var publishStub = sinon.stub(streamClient, "publish");
-	publishStub.resolves();
+		let publishStub = sinon.stub(streamClient, "publish");
+		publishStub.resolves();
 
-	return sendClickEvent(testOutboundUrl)
-		.then(() => {
-			expect(publishStub).to.have.been.calledWith('link-clicked', eventBody);
-		});
+		return sendClickEvent(testOutboundUrl)
+			.then(() => {
+				expect(publishStub).to.have.been.calledWith('link-clicked', eventBody);
+				publishStub.restore();
+			})
+	});
 });

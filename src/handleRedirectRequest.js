8552a82d746438d46432ec0e1404538f5c456f12
Jared Willett <jaredrwillett@gmail.com>
2018-05-04 13:59:14 +1000

    roll sendclickevent into handleredirectrequest

--- src/handleRedirectRequest.js
+++ src/handleRedirectRequest.js
@@ -1,4 +1,11 @@
-const sendClickEvent = require('./sendClickEvent');
+const streamClient = require('./streamClient');
+
+const sendClickEvent = (outboundUrl) => {
+	return streamClient.publish('link-clicked', {
+		outboundUrl: outboundUrl
+	})
+	.catch(() => Promise.reject({ status: 500, message: 'Failed to publish event'}));
+}
 
 const handleRedirectRequest = (req, res) => {
 	if (req.query.outboundUrl) {

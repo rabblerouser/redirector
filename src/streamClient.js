548e1e2c7d9ff90c1bef5bba9580bd63137fe450
Isha Gupta <15913179+guptaisha5484@users.noreply.github.com>
2018-05-12 15:51:54 +1000

    Updated the streamClient configuration with kinesis endpoint

--- src/streamClient.js
+++ src/streamClient.js
@@ -2,6 +2,7 @@ const createClient = require('@rabblerouser/stream-client');
 
 const streamClientSettings = {
   publishToStream: process.env.STREAM_NAME || 'rabblerouser_stream',
+  kinesisEndpoint: process.env.KINESIS_ENDPOINT,
   region: process.env.AWS_REGION || 'ap-southeast-2',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'FAKE',
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'ALSO FAKE',
